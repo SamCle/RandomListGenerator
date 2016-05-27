cdf7aa2bd1135562d999cdfb5eedee6671977302
SamCle <samuele.clerici@csttech.it>
2016-05-27 14:29:32 +0200

    Lerciata to try to improve the use of github

--- src/it/csttech/randomlist/OptionsHandler.java
+++ src/it/csttech/randomlist/OptionsHandler.java
@@ -42,42 +42,7 @@ class OptionsHandler {
     iOptions.put("M", Integer.parseInt(commandLine.getOptionValue("M", "99999")));
     iOptions.put("s", Integer.parseInt(commandLine.getOptionValue("s", " 1000")));
     sOptions.put("f", commandLine.getOptionValue("f", "output.txt")));
-    
 
-    /*    String input = ""; // calcola o legge l'input e ne ricava l'estensione
-    if(cmd.hasOption("s")){
-    input = cmd.getOptionValue("s");
-  } else {
-  input = cmd.getOptionValue("k", "fw");
-  input = "test\\Test" + input.toUpperCase() + "." + input;
-}
-System.out.println(input);
-String extension = input.substring(input.lastIndexOf(".") + 1, input.length());
-
-optionsMap.put("extension", extension);
-optionsMap.put("input", input);
-
-String newExtension = ( extension.equals("csv") ? "fw" : "csv" ); // calcola o legge l'output e ne ricava l'estensione
-newExtension = cmd.getOptionValue("e", newExtension);
-String defaultOutput = input.substring(0, input.lastIndexOf(".")) + "Output" + newExtension.toUpperCase() + "." + newExtension;
-String output = cmd.getOptionValue("o", defaultOutput);
-System.out.println(output);
-
-optionsMap.put("newExtension", newExtension);
-optionsMap.put("output", output);
-
-final String separatorCSV = cmd.getOptionValue("scsv", ";"); // alloca le costanti
-final String boundaryCSV = cmd.getOptionValue("bcsv", "\"");
-final String fillerFW = cmd.getOptionValue("ffw", " ");
-final int standardLengthFW = Integer.parseInt(cmd.getOptionValue("lfw", "-1"));
-
-optionsMap.put("separatorCSV", separatorCSV);
-optionsMap.put("boundaryCSV", boundaryCSV);
-optionsMap.put("fillerFW", fillerFW);
-optionsMap.put("standardLengthFW", new Integer(standardLengthFW).toString());
-
-return optionsMap;
-*/
-}
+  }
 
 }

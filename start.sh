48d17eadbfb9021c35c41696ed79b0ae9e72762e
GabrielePichierri <gabrielepichierri@gmail.com>
2016-06-08 12:08:27 +0200

    changed start.sh as well

--- start.sh
+++ start.sh
@@ -2,10 +2,11 @@
 
 main=RandomListGenerator
 logConfig=cfg/log4j2.xml
+defaultProperties=./cfg/Default.properties
 
 #for i in `seq 1 10000`;
 
 # 	do 
-		java -Dlog4j.configurationFile=$logConfig -cp bin:lib/*  it.csttech.$main $*
+		java -Dlog4j.configurationFile=$logConfig -cp bin:lib/*  it.csttech.$main -dp $defaultProperties $*
 
 #done
